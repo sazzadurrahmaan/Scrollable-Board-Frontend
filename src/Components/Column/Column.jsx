89192009024c09b9a5a106422f23d73a57efaf3c
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-11-07 18:23:53 +0600

    added a comment to describe the code

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -5,7 +5,8 @@ function Column({ title }) {
     <div className="min-w-[300px] mx-2 bg-white rounded-md shadow-md">
       <h3>{title}</h3>
       <div className="max-h-[500px] overflow-y-auto p-2">
-        {[...Array(5)].map((_, index) => (
+        {[...Array(5)].map((_, index) => ( 
+          // Array(5) creates a new array with 5 empty slots[undefined] and (_) means each item
           <Card key={index} />
         ))}
       </div>
