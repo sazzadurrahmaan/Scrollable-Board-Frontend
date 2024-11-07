0cfc2db09c805e5be3de959c5168a601c50a5ad2
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-11-07 18:04:04 +0600

    applied tailwind css to style all markup of the part1 task

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -1,16 +1,16 @@
 import Card from "../Card/Card";
 
 function Column({ title }) {
-    return (
-      <div className="column">
-        <h3>{title}</h3>
-        <div className="column-content">
-          {[...Array(5)].map((_, index) => (
-            <Card key={index} />
-          ))}
-        </div>
+  return (
+    <div className="min-w-[300px] mx-2 bg-white rounded-md shadow-md">
+      <h3>{title}</h3>
+      <div className="max-h-[500px] overflow-y-auto p-2">
+        {[...Array(5)].map((_, index) => (
+          <Card key={index} />
+        ))}
       </div>
-    );
-  }
-  
-  export default Column;
+    </div>
+  );
+}
+
+export default Column;
