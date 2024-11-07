43c312cd57de6f5733cf1ea83d77ec6e88c5ba41
sazzadurrahman-cse <sazzadrn17@gmail.com>
2024-11-07 18:56:24 +0600

    styled Attachment Modal with tailwind

--- src/Components/AttachmentModal/AttachmentModal.jsx
+++ src/Components/AttachmentModal/AttachmentModal.jsx
@@ -17,21 +17,21 @@ function AttachmentModal({ closeModal }) {
     };
 
     return (
-        <Modal isOpen={true} onRequestClose={closeModal} className="modal">
+        <Modal isOpen={true} onRequestClose={closeModal} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 w-[400px] rounded-lg shadow-lg">
             <h2>Attachments</h2>
             <input
                 type="file"
                 multiple
                 onChange={handleFileUpload}
             />
-            <ul className="attachment-list">
+            <ul className="list-none p-0">
                 {attachments.map((file, index) => (
-                    <li key={index}>
-                        {file.name} <span className="file-extension">({file.type.split('/')[1] || 'unknown'})</span>
+                    <li key={index} className='flex justify-between mt-2'>
+                        {file.name} <span className="text-[#888]">({file.type.split('/')[1] || 'unknown'})</span>
                     </li>
                 ))}
             </ul>
-            <button onClick={closeModal} className="close-button">Close</button>
+            <button onClick={closeModal} className="mt-2 px-3 py-2 bg-[#007bff] text-white rounded-md cursor-pointer hover:bg-[#0056b3]">Close</button>
         </Modal>
     );
 }
